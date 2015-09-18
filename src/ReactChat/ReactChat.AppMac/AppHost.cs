7c866cb6d2449f70aa5cccf4672e66ada1761787
Darren Reid <darren.j.reid@gmail.com>
2015-09-18 15:15:07 +1000

    Fix ReactChat.AppMac project

--- src/ReactChat/ReactChat.AppMac/AppHost.cs
+++ src/ReactChat/ReactChat.AppMac/AppHost.cs
@@ -20,32 +20,63 @@ namespace ReactChat.AppMac
 		/// Default constructor.
 		/// Base constructor requires a name and assembly to locate web service classes. 
 		/// </summary>
-		public AppHost()
-			: base("ReactChat.AppMac", typeof(MyServices).Assembly) {}
+		public AppHost ()
+			: base ("ReactChat.AppMac", typeof(ServerEventsServices).Assembly)
+		{
+
+		}
 
 		/// <summary>
 		/// Application specific configuration
 		/// This method should initialize any IoC resources utilized by your web service classes.
 		/// </summary>
 		/// <param name="container"></param>
-		public override void Configure(Container container)
+		public override void Configure (Container container)
 		{
-			//Config examples
-			//this.Plugins.Add(new PostmanFeature());
-			//Plugins.Add(new CorsFeature());
+			JsConfig.EmitCamelCaseNames = true;
 
-			Plugins.Add(new RazorFormat {
+			Plugins.Add(new ServerEventsFeature());
+
+			InitializeAppSettings ();
+
+			Plugins.Add (new RazorFormat {
 				LoadFromAssemblies = { typeof(CefResources).Assembly },
 			});
 
-			SetConfig(new HostConfig {
-				DebugMode = true,
-				EmbeddedResourceBaseTypes = { typeof(AppHost), typeof(CefResources) }
+			SetConfig (new HostConfig {
+				DebugMode = AppSettings.Get("DebugMode", false),
+				DefaultContentType = MimeTypes.Json,
+				AllowFileExtensions = { "jsx" },
+				EmbeddedResourceBaseTypes = { typeof(AppHost), typeof(CefResources) },
 			});
 
 			Routes.Add<NativeHostAction>("/nativehost/{Action}");
 			ServiceController.RegisterService(typeof(NativeHostService));
 
+			CustomErrorHttpHandlers.Remove(HttpStatusCode.Forbidden);
+
+			//Register all Authentication methods you want to enable for this web app.            
+			Plugins.Add(new AuthFeature(
+				() => new AuthUserSession(),
+				new IAuthProvider[] {
+					new TwitterAuthProvider(AppSettings)   //Sign-in with Twitter
+				}));
+
+			container.RegisterAutoWiredAs<MemoryChatHistory, IChatHistory>();
+
+			var redisHost = AppSettings.GetString("RedisHost");
+			if (redisHost != null)
+			{
+				container.Register<IRedisClientsManager>(new RedisManagerPool(redisHost));
+
+				container.Register<IServerEvents>(c =>
+					new RedisServerEvents(c.Resolve<IRedisClientsManager>()));
+				container.Resolve<IServerEvents>().Start();
+			}
+		}
+
+		private void InitializeAppSettings()
+		{
 			var allKeys = AppSettings.GetAllKeys();
 			if (!allKeys.Contains("platformsClassName"))
 				AppSettings.Set("platformsClassName", "mac");
@@ -53,23 +84,35 @@ namespace ReactChat.AppMac
 				AppSettings.Set("PlatformCss", "mac.css");
 			if (!allKeys.Contains("PlatformJs"))
 				AppSettings.Set("PlatformJs", "mac.js");
+
+			if(!allKeys.Contains("oauth.RedirectUrl"))
+				AppSettings.Set("oauth.RedirectUrl", MainClass.HostUrl);
+			if(!allKeys.Contains("oauth.CallbackUrl"))
+				AppSettings.Set("oauth.CallbackUrl", MainClass.HostUrl + "auth/{0}");
+			if(!allKeys.Contains("oauth.twitter.ConsumerKey"))
+				AppSettings.Set("oauth.twitter.ConsumerKey", "6APZQFxeVVLobXT2wRZArerg0");
+			if (!allKeys.Contains("oauth.twitter.ConsumerSecret"))
+				AppSettings.Set("oauth.twitter.ConsumerSecret", "bKwpp31AS90MUBw1s1w0pIIdYdVEdPLa1VvobUr7IXR762hdUn");
+			//if (!allKeys.Contains("RedisHost"))
+			//    AppSettings.Set("RedisHost", "localhost:6379");
 		}
 	}
 
 	public class NativeHostService : Service
 	{
-		public object Get(NativeHostAction request)
+		public object Get (NativeHostAction request)
 		{
-			if (string.IsNullOrEmpty(request.Action)) 
+			if (string.IsNullOrEmpty (request.Action)) {
 				throw HttpError.NotFound ("Function Not Found");
-
-			var nativeHost = typeof(NativeHost).CreateInstance<NativeHost>();
-			var methodName = request.Action.First ().ToString ().ToUpper() + string.Join ("", request.Action.Skip(1));
-			var methodInfo = typeof(NativeHost).GetMethod(methodName);
-			if (methodInfo == null)
-				throw new HttpError(HttpStatusCode.NotFound,"Function Not Found");
-
-			methodInfo.Invoke(nativeHost, null);
+			}
+			Type nativeHostType = typeof(NativeHost);
+			object nativeHost = nativeHostType.CreateInstance<NativeHost> ();
+			string methodName = request.Action.First ().ToString ().ToUpper () + String.Join ("", request.Action.Skip (1));
+			MethodInfo methodInfo = nativeHostType.GetMethod (methodName);
+			if (methodInfo == null) {
+				throw new HttpError (HttpStatusCode.NotFound, "Function Not Found");
+			}
+			methodInfo.Invoke (nativeHost, null);
 			return null;
 		}
 	}
@@ -81,21 +124,21 @@ namespace ReactChat.AppMac
 
 	public class NativeHost
 	{
-		public void ShowAbout()
+		public void ShowAbout ()
 		{
 			//Invoke native about menu item programmatically.
 			MainClass.MainMenu.InvokeOnMainThread (() => {
 				foreach (var item in MainClass.MainMenu.ItemArray()) {
 					if (item.Title == "ReactChat") {
-						item.Submenu.PerformActionForItem(0);
+						item.Submenu.PerformActionForItem (0);
 					}
 				}
 			});
 		}
 
-		public void Quit()
+		public void Quit ()
 		{
-			Environment.Exit(0);
+			Environment.Exit (0);
 		}
 	}
 }
